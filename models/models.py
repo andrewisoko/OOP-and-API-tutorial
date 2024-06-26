7fef3aa460146b7646d1bed29f5a22e95bcd09d9
Kesler <uchekesla@gmail.com>
2024-06-26 21:18:36 +0100

    Add new fields to User class and implement save_to_db function

--- models/models.py
+++ models/models.py
@@ -6,18 +6,24 @@ import json
 #              #
 # =============#
 
+
 class Meme:
     def __init__(self, url):
         self.url = url
 
+
 class User:
-    def __init__(self, name):
+    def __init__(self, name, age, image):
         self.name = name
+        self.age = age
+        self.image= image
+
 
 class Message:
     def __init__(self, text, author):
         self.text = text
         self.author = author
+
 
 # ================#
 #                 #
@@ -25,13 +31,53 @@ class Message:
 #                 #
 # ================#
 
-def save_to_db(object):
 
+def save_to_db(object, object_name):
+    """Save objects to the database. As a JSON file.
+
+      # Example
+      ```python
+      message = Message("This is a messsage", "Kesler")
+      save_to_db(message, "message")
+      ```
+
+      # Note
+      The database structure is the following
+      ```json
+      {
+        "user" : [
+          { "name" : "Andrew" },
+          { "name" : "Kesler"}
+        ],
+        "message" : [
+          { "author" : "Kesler", "text" : "This is a message" }
+        ]
+      }
+    ```
+    """
+    # Get the dictionary representation of the object.
     model = object.__dict__
-    
-    with open("db.json", "w") as f:
-        f.write(object)
 
+    # Open the current json file.
+    with open("db.json", "r") as db_file:
+
+        # Load the json dictionary.
+        current_db = json.load(db_file)
+
+        current_entries = current_db.get(object_name, [])
+
+        # List with the new model
+        current_entries.append(model)
+
+        current_db[object_name] = current_entries
+        new_db = current_db
+
+    # Write the dictionary representation to a json file.
+    with open("db.json", "w") as db_file:
+        db_file.write(json.dumps(new_db))
+
+
+def read_from_db():
 
 # ================#
 #                 #
@@ -40,17 +86,23 @@ def save_to_db(object):
 # ================#
 
 
-def create_user(name):
-    user = User(name)
+def create_user(name, age, image):
+    user = User(name, age, image)
+
+    save_to_db(user, "user")
 
 
 def create_message(text, author):
-    return Message(text, author)
+    message = Message(text, author)
+    save_to_db(message, "message")
 
 
-def main():
-    create_user("Kesler")
+# ====================#
+#                     #
+#   CALL FUNCTIONS    #
+#                     #
+# ====================#
 
 
-if __name__ == "__main__":
-    main()
+
+# create_message("This is the text", "Kesler")
